41194c6cbb0c567e886d8d3a3919eb4307cf4ffc
xdgamer90000 <xdgamer90000@gmail.com>
2024-05-06 00:37:55 -0400

    1.1.1

--- src/main/java/me/xdgamer90000/botwows/commands/EndContest.java
+++ src/main/java/me/xdgamer90000/botwows/commands/EndContest.java
@@ -45,16 +45,16 @@ public class EndContest extends SlashCommands {
                 e.printStackTrace();
             }
 
-            event.reply(new MessageCreateBuilder().addContent("The Monthly Contest has come to an end, :MaltaWUT: :MaltaPog: \n\nWinners: :MaltaStonks: \n\n" +
+            event.reply(new MessageCreateBuilder().addContent("The Monthly Contest has come to an end,\n\nWinners:\n\n" +
                     "1st Place: <@" +winners.get(0).userID+"> Using: "+winners.get(0).shipUsed+" Score: "+winners.get(0).score+" Server: "+winners.get(0).server+" "+division(winners.get(0).division)+"\n"
                     +"2nd Place: <@"+winners.get(1).userID+"> Using: "+winners.get(1).shipUsed+" Score: "+winners.get(1).score+" Server: "+winners.get(1).server+" "+division(winners.get(1).division)+"\n"
                     +"3rd Place: <@"+winners.get(2).userID+"> Using: "+winners.get(2).shipUsed+" Score: "+winners.get(2).score+" Server: "+winners.get(2).server+" "+division(winners.get(2).division)+"\n"
                     +"Raffle Winner: <@"+winners.get(3).userID+"> Using: "+winners.get(3).shipUsed+" Score: "+winners.get(3).score+" Server: "+winners.get(3).server+" "+division(winners.get(3).division)+"\n"
                     +"Raffle Winner: <@"+winners.get(4).userID+"> Using: "+winners.get(4).shipUsed+" Score: "+winners.get(4).score+" Server: "+winners.get(4).server+" "+division(winners.get(4).division)+"\n"
                     +"Raffle Winner: <@"+winners.get(5).userID+"> Using: "+winners.get(5).shipUsed+" Score: "+winners.get(5).score+" Server: "+winners.get(5).server+" "+division(winners.get(5).division)+"\n"
-                    +"\n\n(All data is compiled by <@378342991410102272> developed by <@362055945121038343>)\n\nCongratulations to all who have taken part, :MaltaGrober: \n"
+                    +"\n(All data is compiled by <@378342991410102272> developed by <@362055945121038343>)\n\nCongratulations to all who have taken part.\n"
                     +"Please private message <@278984293135679488> to claim your prize, the codes will be sent to you by private message\n" +
-                    "New contest will be announced at a later date. :MaltaZao: ").build()).queue();
+                    "New contest will be announced at a later date.").build()).queue();
         }else{
             event.reply("You do not have the authority to end a contest").queue();
         }
